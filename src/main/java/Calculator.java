4e6121d057cd946d6157c0a575b7941fc8a717d0
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-05 23:50:44 +0100

    Add multiply test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,14 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
     public int sum(List<String> numbers){
         throw new NotImplementedException();
+    }
+
+    public int multiply(ArrayList<String> numbers) {
+        throw new UnsupportedOperationException();
     }
 }
