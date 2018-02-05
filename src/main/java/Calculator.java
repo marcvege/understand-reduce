295564951d7c0f784c3aaa5671381e9ce739dca2
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-05 23:30:22 +0100

    Sum only one element

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,6 +4,6 @@ public class Calculator {
     public int sum(List<String> numbers) {
         if (numbers.isEmpty())
             return 0;
-        return 0;
+        return Integer.parseInt(numbers.get(0));
     }
 }
