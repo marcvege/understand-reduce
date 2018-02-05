3d807ada643f15d20fa917c88c08268ab7e9a265
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-06 00:05:50 +0100

    Reorganize imports

--- src/main/java/Obfuscator.java
+++ src/main/java/Obfuscator.java
@@ -1,10 +1,10 @@
-import org.apache.commons.lang.StringUtils;
-
 import java.util.List;
+
+import static org.apache.commons.lang.StringUtils.repeat;
 
 public class Obfuscator {
     public String obfuscate(String data, List<String> masks) {
         return masks.stream()
-                .reduce(data, (obfuscated, mask) -> obfuscated.replace(mask, StringUtils.repeat("*", mask.length())));
+                .reduce(data, (obfuscated, mask) -> obfuscated.replace(mask, repeat("*", mask.length())));
     }
 }
