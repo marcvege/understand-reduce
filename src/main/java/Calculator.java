03b54181168a2bc8ba3b42d065a66d28476927a5
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-05 23:37:11 +0100

    Remove unnecessary guard clause

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,12 +2,9 @@ import java.util.List;
 
 public class Calculator {
     public int sum(List<String> numbers) {
-        if (numbers.isEmpty())
-            return 0;
         return numbers.stream()
                 .map(Integer::parseInt)
                 .reduce((i1, i2) -> i1 + i2)
                 .orElse(0);
-
     }
 }
