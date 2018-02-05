d450f9c4a761096faffd41aabbca28c130e6065e
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-05 23:39:16 +0100

    Exclude no numeric operands

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,11 @@
+import org.apache.commons.lang.StringUtils;
+
 import java.util.List;
 
 public class Calculator {
     public int sum(List<String> numbers) {
         return numbers.stream()
+                .filter(StringUtils::isNumeric)
                 .map(Integer::parseInt)
                 .reduce((i1, i2) -> i1 + i2)
                 .orElse(0);
