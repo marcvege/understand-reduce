4bcebd29c1cd623aadf83835b711b2950ff59a41
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-05 23:36:19 +0100

    Sum numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
     public int sum(List<String> numbers) {
         if (numbers.isEmpty())
             return 0;
-        return Integer.parseInt(numbers.get(0));
+        return numbers.stream()
+                .map(Integer::parseInt)
+                .reduce((i1, i2) -> i1 + i2)
+                .orElse(0);
+
     }
 }
