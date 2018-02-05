6363a311ab648e239154f665aa6f6304dc70d7c6
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-05 23:55:28 +0100

    Refactor

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,24 +1,24 @@
 import org.apache.commons.lang.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class Calculator {
     public int sum(List<String> numbers) {
-        return numbers.stream()
-                .filter(StringUtils::isNumeric)
-                .filter(StringUtils::isNotEmpty)
-                .map(Integer::parseInt)
-                .reduce((i1, i2) -> i1 + i2)
-                .orElse(0);
+        return apply(numbers, (i1, i2) -> i1 + i2);
     }
 
     public int multiply(List<String> numbers) {
+        return apply(numbers, (i1, i2) -> i1 * i2);
+    }
+
+    private Integer apply(List<String> numbers, BinaryOperator<Integer> sum) {
         return numbers.stream()
                 .filter(StringUtils::isNumeric)
                 .filter(StringUtils::isNotEmpty)
                 .map(Integer::parseInt)
-                .reduce((i1, i2) -> i1 * i2)
+                .reduce(sum)
                 .orElse(0);
     }
+
 }
