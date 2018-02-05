c7db0466fe1c0819cd0d2cf7e37cd6b987d4289e
Marc Villagrasa <marc.villagrasa@mango.com>
2018-02-05 23:05:23 +0100

    Use StringUtils

--- src/main/java/Obfuscator.java
+++ src/main/java/Obfuscator.java
@@ -1,23 +1,10 @@
+import org.apache.commons.lang.StringUtils;
+
 import java.util.List;
-import java.util.function.BinaryOperator;
 
 public class Obfuscator {
-    public String obfuscate(String data, List<String> masks){
-        BinaryOperator<String> function = new BinaryOperator<String>() {
-            public String apply(String data, String mask) {
-                return data.replace(mask, repeat('*', mask.length()));
-            }
-
-        };
+    public String obfuscate(String data, List<String> masks) {
         return masks.stream()
-                .reduce(data, function);
-    }
-
-    private String repeat(char c, int length) {
-        char[] repeated = new char[length];
-        for (int i = 0; i < length; i++) {
-            repeated[i] = c;
-        }
-        return String.valueOf(repeated);
+                .reduce(data, (obfuscated, mask) -> obfuscated.replace(mask, StringUtils.repeat("*", mask.length())));
     }
 }
--- src/test/java/ObfuscatorTest.java
+++ src/test/java/ObfuscatorTest.java
@@ -15,7 +15,8 @@ class ObfuscatorTest {
     @DisplayName("Positives cases")
     @ParameterizedTest(name = "\"{0}\" should be {2}")
     @CsvSource({"param1:data1, data1, param1:*****",
-            "param1:data1 param2:data2 param3:data3, data1|data3, param1:***** param2:data2 param3:*****"})
+            "param1:data1 param2:data2 param3:data3, data1|data3, param1:***** param2:data2 param3:*****",
+            "param1:data1 param2:data2 , data1||data3, param1:***** param2:data2"})
     void mask_data(String dataToMask, String packedMasks, String dataExpected) {
         String dataMasked = obfuscator.obfuscate(dataToMask, getMasks(packedMasks));
 
